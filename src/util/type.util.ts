fd40339021a3848670b7a0edfff61209c8dbf0e9
mav <artyom.maklakov@nuorder.com>
2020-05-03 23:18:54 +0300

    feat(types-util): add `WithRequired` type to make part of interface required

--- src/util/type.util.ts
+++ src/util/type.util.ts
@@ -3,6 +3,8 @@ export type PropType<TObj, TProp extends keyof TObj> = TObj[TProp]
 type Optional<T, K> = { [P in Extract<keyof T, K>]?: T[P] }
 export type WithOptional<T, K extends keyof T> = T extends never ? never : Omit<T, K> & Optional<T, K>
 
+export type WithRequired<T, K extends keyof T> = T extends never ? never : Partial<T> & Required<Pick<T, K>>
+
 /**
  * For instance we have such interface
  *
