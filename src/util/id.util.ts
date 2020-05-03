7a71d941eeea714a8e446afce212e81ddba1f5b6
mav <artyom.maklakov@nuorder.com>
2020-05-03 17:46:38 +0300

    feat(util): add `create hash` fn

--- src/util/id.util.ts
+++ src/util/id.util.ts
@@ -1,3 +1,8 @@
 import { nanoid } from 'nanoid'
+import crypto from 'crypto'
 
 export const generateId = (): string => nanoid()
+
+export const createHash = (str: string): string => {
+  return crypto.createHash('sha1').update(str).digest('base64')
+}
